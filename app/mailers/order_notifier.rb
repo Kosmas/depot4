82220abb917df02661618a06b58f0340fae09c43
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-06-23 15:46:19 +0200

    added email content tests

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -1,5 +1,5 @@
 class OrderNotifier < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "depot@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -9,7 +9,7 @@ class OrderNotifier < ActionMailer::Base
   def received(order)
     @order = order
 
-    mail to: order.email, subject: 'Pragmatic Store Order Confirmatin'
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
