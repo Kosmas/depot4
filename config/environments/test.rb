fbf19d0755bcc7ca4481be52ee86a9f8d350b432
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-12-27 12:40:53 +0100

    config/environments/production.rb
    renamed config_static_assets to config_static_files for deprecation message and test_order to random

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,4 +36,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Added after upgrade to rails 4.2.0
+  config.active_support.test_order = :random
 end
